3a904fb77ec90550e337f34497ef7f87e712a7a4
Pannn12 <alfanpartll@gmail.com>
2024-01-12 20:16:26 +0700

    second commit

---
+++ src/consts.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Alfan Hidayat';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const HOME_TITLE = 'Home | Alfan'
+export const BLOG_TITLE = 'Blog | Alfan'
+export const ABOUT_TITLE = 'About | Alfan'
