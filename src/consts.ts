128c933545e0e1812f15e4cfb985ef9e7e3e9007
Pannn12 <alfanpartll@gmail.com>
2024-01-23 07:15:52 +0700

    three commit

--- src/consts.ts
+++ src/consts.ts
@@ -6,3 +6,12 @@ export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const HOME_TITLE = 'Home | Alfan'
 export const BLOG_TITLE = 'Blog | Alfan'
 export const ABOUT_TITLE = 'About | Alfan'
+
+export const ABOUT_EVERYONE = 'Hi, Everyone!'
+export const ABOUT_WEBDEV = 'Alfan Hidayat | Web Developer'
+export const ABOUT_DESCRIPTION = 'I am a student in the Informatics Engineering Study Program at Pamulang University. I am currently learning in the world of web development, especially in the field of front-end development.'
+export const ABOUT_CONTACT = 'Contact me via'
+export const MYINSTAGRAM = '@kelraskie'
+export const MYEMAIL = 'alfanhidayat12321@gmail.com'
+export const ABOUT_CLICKIMAGE = 'You can contact me via email & instagram, or click image.'
+export const ABOUT_THANKYOU = 'Thank Youu!👋‎‎'
